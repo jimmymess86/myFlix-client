0543d712ed6bc9e468255d43b9d14c92a6326d33
jimmymess86 <jimmymess86@gmail.com>
2022-03-01 13:29:25 -0500

    registration,login,PropTypes added
    task 3.4 AdvancedReact

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 
+import { RegistrationView } from '../registration-view/registration-view';
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -8,16 +10,23 @@ export class MainView extends React.Component {
 
     constructor() {
         super();
+        // initial state is set to null
         this.state = {
             movies: [],
-            selectedMovie: null
+            selectedMovie: null,
+            user: null
         }
     }
 
     componentDidMount() {
         let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjFkMmRhZjQ3ZWNkZTU1NmFhMmIyYWEiLCJVc2VybmFtZSI6ImpvbmRvZTEiLCJQYXNzd29yZCI6IiQyYiQxMCQ5ZkVOc1lzTmg4b0FFcVZSN2xXMDBldm9vLnplUjVEUjk1aDF5MUpuVkhpbHdHamJSNzZ4UyIsIkVtYWlsIjoibmV3am9uZG9lQGVtYWlsLmNvbSIsIkJpcnRoZGF5IjoiMjAxMi0xMS0xMVQwMDowMDowMC4wMDBaIiwiRmF2b3JpdGVNb3ZpZXMiOltdLCJfX3YiOjAsImlhdCI6MTY0NjA3OTQxMywiZXhwIjoxNjQ2Njg0MjEzLCJzdWIiOiJqb25kb2UxIn0.Eoig4YUc4VpSh-N7HDabzVgHnMccvmgtZ2yIsIRC4yc";
-        axios.get('https://mclaughlinflixdb.herokuapp.com/movies', {
-            headers: {
+        axios.get('https://mclaughlinflixdb.herokuapp.com/movies')
+            .then(response => {
+                this.setState({
+                    movies: response.data
+                });
+            })
+            /*headers: {
                 Authorization: 'Bearer ' + token
             }
         })
@@ -25,11 +34,14 @@ export class MainView extends React.Component {
                 this.setState({
                     movies: response.data
                 });
-            })
+            })*/
             .catch(error => {
                 console.log(error);
             });
     }
+
+    /* when a movie is clicked, this fucntion is invoked and updates thes tate of the 'selectedMovie' *property to that movie
+    */
 
     setSelectedMovie(newSelectedMovie) {
         this.setState({
@@ -37,13 +49,38 @@ export class MainView extends React.Component {
         });
     }
 
+    onRegistration(register) {
+        this.setState({
+            register
+        });
+    }
+
+    /* When a user successfully logs in, this function updates the 'user' property in state to that *particular username*
+    */
+
+    onLoggedIn(user) {
+        this.setState({
+            user
+        });
+    }
+
     render() {
-        const { movies, selectedMovie } = this.state;
+        const { movies, selectedMovie, user, register } = this.state;
+
+        if (!register) return (<RegistrationView onRegistration={(register) => this.onRegistration(register)} />)
+
+        /* if there is no user, the LoginView is rednered. If there is a user logged in, the user details are *passed as a prop to the LoginView
+        */
+        if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+
+        // Before the movies have been loaded
 
         if (movies.length === 0) return <div className="main-view" />;
 
         return (
             <div className="main-view">
+                {/* if the state of 'selectedMovie/ is not null, that selected movie will be returned otherwise, all *movies will be returned
+                */}
                 {selectedMovie
                     ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
                     : movies.map(movie => (
